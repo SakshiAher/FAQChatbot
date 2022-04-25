4ba4c600deb9a1d5851ef8d474fd60561313ee6a
SakshiAher <101646778+SakshiAher@users.noreply.github.com>
2022-04-25 21:47:40 +0530

    Add files via upload

---
+++ demofaqchatbot-bot-src (4)/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\demofaqchatbot-bot-96f8-Web-Deploy.pubxml -p:Password=NGANKTY0t4qZQrgB5w8Yyv9Yfrk8k1pLhxliJRExmdg0Ciz0G69rLxk6jtT6
+
